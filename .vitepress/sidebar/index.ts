9a388f259cdd0529ee85025a956b7424bcbe6f60
卢国祥 <noob-coder@qq.com>
2025-11-03 03:49:40 +0800

    feat(sidebar): 添加考研数学手册侧边栏配置
    - 导入考研数学手册侧边栏模块
    - 配置考研数学手册路由路径与基础路径
    - 注释掉原有的计算机网络侧边栏配置- 调整侧边栏配置项的顺序以优化结构

--- .vitepress/sidebar/index.ts
+++ .vitepress/sidebar/index.ts
@@ -1,12 +1,13 @@
 import {sidebar408ComputerNetwork} from "./408.mjs"
+import {kaoYanPamphlet} from "./math.mjs";
 
 export const sidebar = {
-    '/408/computer-network/': {base: '/408/computer-network/', items: sidebar408ComputerNetwork()},
+    '/math/kaoyan-pamphlet/': {base: '/math/kaoyan-pamphlet/', items: kaoYanPamphlet()},
+    // '/408/computer-network/': {base: '/408/computer-network/', items: sidebar408ComputerNetwork()},
     /*'/408/data-structure/': {base: '/408/data-structure/', items: sidebar408DataStructure()},
     '/php/composer/': {base: '/php/composer/', items: sidebarPHPComposer()},
     '/php/php-fig/psr/': {base: '/php/php-fig/psr/', items: sidebarPSR()},
     '/php/php-best-practices/': {base: '/php/php-best-practices/', items: sidebarPhpBestPractices()},
-    '/math/kaoyan-pamphlet/': {base: '/math/kaoyan-pamphlet/', items: kaoYanPamphlet()},
     '/database/mysql/references/': {
         base: '/database/mysql/references/',
         items: sidebarDatabaseMysqlReferences()
